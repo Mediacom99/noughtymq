f15247478588a96d5e3c14603a87273453df0de9
Edoardo <edoardo@kali-vm.homenetwork.local>
2024-04-28 17:39:38 +0200

    added build script for linux systems

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+#
+# Build script
+#
+
+if [ $# -eq 0 ]
+then
+	echo "No arguments provided!"
+	echo "please provide source file name and output file name"
+	exit
+fi
+clang -v -L/usr/lib/x86_64-linux-gnu -lzmq $1 -o $2
+exit
